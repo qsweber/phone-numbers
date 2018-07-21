d963599d058134786e3a49bd4a5e6412e8005a38
quinn <quinn@quinnweber.com>
2018-07-21 13:37:23 -0700

    add new match

--- phone_numbers/app.py
+++ phone_numbers/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 from phone_numbers.words import WORDS
+from phone_numbers.lib.words import get_new_words
 from phone_numbers.lib.trie import Trie
 from phone_numbers.lib.sentence_creator import make_sentence_from_numbers
 
@@ -10,14 +11,17 @@ app = Flask(__name__)
 @app.route('/api/v0/match', methods=['GET'])
 def index():
     trie = Trie(WORDS)
+    trie_new = Trie(get_new_words(WORDS))
 
     string = request.args['value']
 
     match = make_sentence_from_numbers(trie, string)
+    match_new = make_sentence_from_numbers(trie_new, string)
 
     response = jsonify({
         'input': string,
         'match': match,
+        'match_new': match_new,
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
--- phone_numbers/tests/lib/test_sentence_creator.py
+++ phone_numbers/tests/lib/test_sentence_creator.py
@@ -4,11 +4,13 @@ import string
 import pytest
 
 from phone_numbers.words import WORDS
+from phone_numbers.lib.words import get_new_words
 from phone_numbers.lib.trie import Trie
 from phone_numbers.lib import sentence_creator as module
 
 
 trie = Trie(WORDS)
+trie_new = Trie(get_new_words(WORDS))
 
 
 @pytest.mark.parametrize(
@@ -41,8 +43,23 @@ def test_make_sentence_from_numbers(string, expected):
     assert actual == expected
 
 
+@pytest.mark.parametrize(
+    'string, expected',
+    [
+        ('2679809273', 'copy t0ward'),
+        ('0003313039', '00 0dd 1d 0 ex'),
+        ('0000000000', '0000000000'),
+        ('1800837863', '1t 0 0vert me'),
+    ],
+)
+def test_make_sentence_from_numbers_trie_new(string, expected):
+    actual = module.make_sentence_from_numbers(trie_new, string)
+
+    assert actual == expected
+
+
 def test_never_errors():
-    for i in range(100):
+    for i in range(10):
         random_numbers_as_string = ''.join(random.choices(list(string.digits), k=10))
         actual = module.make_sentence_from_numbers(trie, random_numbers_as_string)
         assert len(actual) >= len(random_numbers_as_string)
